6a360c4f12ee628689bf861051c987bbd09ed2ca
MexNL <mex_kelters@hotmail.com>
2025-05-10 23:11:46 +0200

    eerste helper functie is gemaakt

---
+++ src/helpers/amountSold.js
@@ -0,0 +1,9 @@
+import {inventory} from '../constants/inventory.js';
+function amountSold(){
+    let amount = 0;
+    for (let i = 0; i < inventory.length; i++){
+        amount += inventory[i].sold;
+    }
+    return amount
+}
+export default amountSold;
